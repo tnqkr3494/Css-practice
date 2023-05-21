1231c1049c684d054dabfdcb8cb1e32030cd2c3f
tnqkr3494 <tnqkr3494@naver.com>
2023-05-21 13:41:12 +0900

    Folio_2
    box-sizing : border-box;
    패딩과 테두리를 포함하여 크기계산!!!

--- UI/folio.js
+++ UI/folio.js
@@ -1,11 +1,51 @@
 const body = document.querySelector("body");
 const about = document.querySelector(".about");
-const close = document.querySelector("#about-section i");
+const contact = document.querySelector(".contact");
+const travel = document.querySelector(".travel");
+const wildlife = document.querySelector(".wildlife");
+const nature = document.querySelector(".nature");
+const about_close = document.querySelector("#about-section i");
+const contact_close = document.querySelector("#contact-section i");
+const travel_close = document.querySelector("#travel-section i");
+const wildlife_close = document.querySelector("#wildlife-section i");
+const nature_close = document.querySelector("#nature-section i");
 
 about.addEventListener("click", () => {
   body.classList.add("about-on");
 });
 
-close.addEventListener("click", () => {
+contact.addEventListener("click", () => {
+  body.classList.add("contact-on");
+});
+
+travel.addEventListener("click", () => {
+  body.classList.add("travel-on");
+});
+
+wildlife.addEventListener("click", () => {
+  body.classList.add("wildlife-on");
+});
+
+nature.addEventListener("click", () => {
+  body.classList.add("nature-on");
+});
+
+about_close.addEventListener("click", () => {
   body.classList.remove("about-on");
+});
+
+contact_close.addEventListener("click", () => {
+  body.classList.remove("contact-on");
+});
+
+travel_close.addEventListener("click", () => {
+  body.classList.remove("travel-on");
+});
+
+wildlife_close.addEventListener("click", () => {
+  body.classList.remove("wildlife-on");
+});
+
+nature_close.addEventListener("click", () => {
+  body.classList.remove("nature-on");
 });
