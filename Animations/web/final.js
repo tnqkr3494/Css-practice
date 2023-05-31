81643b4d6a00d27af5d2d7f7cdefb6309c0d6753
tnqkr3494 <tnqkr3494@naver.com>
2023-06-01 00:01:37 +0900

    car_slider

--- Animations/web/final.js
+++ Animations/web/final.js
@@ -32,3 +32,60 @@ document.querySelector("#login-btn").onclick = () => {
 document.querySelector("#close-login-btn").onclick = () => {
   document.querySelector(".login-form-container").classList.remove("active");
 };
+
+document.querySelector(".home").onmousemove = (e) => {
+  document.querySelectorAll(".home-parallax").forEach((element) => {
+    let speed = element.getAttribute("data-speed");
+
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
+
+    element.style.transform = `translateX(${y}px) translateY(${x}px)`;
+  });
+};
+
+document.querySelector(".home").onmouseleave = (e) => {
+  document.querySelectorAll(".home-parallax").forEach((element) => {
+    element.style.transform = `translateX(0px) translateY(0px)`;
+  });
+};
+
+let wrapper = document.querySelector(".wrapper-box");
+let activeBox = document.querySelectorAll(".box");
+let activeLabel = document
+  .querySelector(".activeCircle")
+  .querySelectorAll(".fa-circle");
+let nextBtn = document.querySelector("#nextBtn");
+let preBtn = document.querySelector("#preBtn");
+
+let indexNo = 0;
+
+nextBtn.onclick = () => {
+  indexNo++;
+  changeBox();
+};
+
+preBtn.onclick = () => {
+  indexNo--;
+  changeBox();
+};
+
+let changeBox = () => {
+  if (indexNo > activeBox.length - 1) {
+    indexNo = 0;
+  } else if (indexNo < 0) {
+    indexNo = activeBox.length - 1;
+  }
+
+  for (let i = 0; i < activeBox.length; i++) {
+    if (i === indexNo) {
+      activeBox[i].classList.add("active");
+      activeLabel[i].classList.add("fa-solid");
+
+      wrapper.style.transform = `translateX(${indexNo * -250}px)`;
+    } else {
+      activeBox[i].classList.remove("active");
+      activeLabel[i].classList.remove("fa-solid");
+    }
+  }
+};
